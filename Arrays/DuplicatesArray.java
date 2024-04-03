375915f063d84ccba4b78ba087a3fde631e49a0d
BeSingularTeam <besingulardemo@gmail.com>
2024-04-03 11:24:55 +0530

    firstcommit

--- Arrays/DuplicatesArray.java
+++ Arrays/DuplicatesArray.java
@@ -7,7 +7,7 @@ class Main{
         int index =1;
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
-                if(arr[j]==arr[j]){
+                if(arr[j]==arr[i]){
                     break;
                 }
             }
