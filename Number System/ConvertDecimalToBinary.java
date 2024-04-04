b387f783fa562e28ffabfd608669301048154552
BeSingularTeam <besingulardemo@gmail.com>
2024-04-04 20:29:36 +0530

    DecimalToBinary

---
+++ Number System/ConvertDecimalToBinary.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        int pow = 1;
+        int[] binary = new int[32];
+        int i=0;
+        while(n>0){
+            binary[i] = n%2;
+            i++;
+            n/=2;
+        }
+        for(int ind=i-1;ind>=0;ind--){
+            System.out.println(binary[ind]);
+        }
+    }
+}
