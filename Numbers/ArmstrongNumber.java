e675533927564c60e734f4dcf87fce6d86fdd62a
BeSingularTeam <besingulardemo@gmail.com>
2024-04-06 19:01:07 +0530

    ArmstrongNumber

---
+++ Numbers/ArmstrongNumber.java
@@ -0,0 +1,27 @@
+class Main{
+    public static boolean ArmstrongNumber(int n){
+        int originalNumber = n;
+        int count = 0;
+        int temp = n;
+        while(temp!=0){
+            count++;
+            temp/=10;
+        }
+        int sum = 0;
+        while(n!=0){
+            int digit = n%10;
+            sum+=Math.pow(digit,count);
+            n/=10;
+        }
+        return (sum==originalNumber);
+    }
+    public static void main(String[] args){
+        int n=153;
+        if(ArmstrongNumber(n)){
+            System.out.println("Armstrong Number");
+        }
+        else{
+            System.out.println("Not Armstrong Number");
+        }
+    }
+}
