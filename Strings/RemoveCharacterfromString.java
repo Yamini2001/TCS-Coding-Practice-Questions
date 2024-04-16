5ec9210eaf66f97f4e5a3f040fd159bfb286eb23
BeSingularTeam <besingulardemo@gmail.com>
2024-04-17 00:31:22 +0530

    removeCharacterExceptalphabet

---
+++ Strings/RemoveCharacterfromString.java
@@ -0,0 +1,22 @@
+public class tUf {
+  public static String solve(String str, int n) {
+    StringBuffer ans = new StringBuffer();
+
+    for (int i = 0; i < n; i++) {
+      int ascii = (int) str.charAt(i); //ascii value
+
+      if ((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122)) // if alphabets
+        ans.append(str.charAt(i));
+    }
+
+    return ans.toString();
+  }
+  public static void main(String args[]) {
+    String str = "take12% *&u ^$#forward";
+    int n = str.length();
+
+    System.out.println("Resultant string:");
+    System.out.println(solve(str, n));
+  }
+}
+
