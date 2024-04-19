e17ea7badabfa97cd2d9e9af78caf75354efa919
BeSingularTeam <besingulardemo@gmail.com>
2024-04-19 15:32:12 +0530

    ChangeLetterLexicographicOrder

---
+++ Strings/ChangeLetterwithLexicographicOrder.java
@@ -0,0 +1,26 @@
+public class Main {
+  public static String solve(String str, int length) {
+    StringBuffer ans = new StringBuffer();
+    for (int i = 0; i < length; i++) {
+      int ascii = (int)(str.charAt(i));
+      if (ascii == 90)
+        ans.insert(i, (char)(65));
+
+      else if (ascii == 122)
+        ans.insert(i, (char)(97));
+
+      else if ((ascii >= 65 && ascii < 90) || (ascii >= 97 && ascii < 122))
+        ans.insert(i, (char)(ascii + 1));
+    }
+    return ans.toString();
+  }
+
+  public static void main(String args[]) {
+    String str = "abcdxyz";
+    int length = str.length();
+    System.out.println("Original string: ");
+    System.out.println(str);
+    System.out.println("New string: ");
+    System.out.println(solve(str, length));
+  }
+}
