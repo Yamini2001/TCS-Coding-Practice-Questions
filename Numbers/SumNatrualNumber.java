949b46b68d86cd5bc9d19a9cac514b1f6b51db4f
BeSingularTeam <besingulardemo@gmail.com>
2024-04-23 20:32:56 +0530

    SumNaturalNumber

--- Numbers/SumNatrualNumber.java
+++ Numbers/SumNatrualNumber.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+
 class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int n = sc.nextInt();
-        int sum = n * (n+1)/2;
-        System.out.println("Sum of first natural numbers: "+sum);
+        float sum = (float) (n * (n + 1)) /2;
+        System.out.println("Sum of the natural numbers: "+sum);
     }
 }
