fc0f4c4caa7c174d4a67dfacf0d72355650ecf01
BeSingularTeam <besingulardemo@gmail.com>
2024-04-26 00:21:18 +0530

    BinarySearch

---
+++ Sorting/BinarySearch.java
@@ -0,0 +1,31 @@
+class Main {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Target found
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11, 13, 15};
+        int target = 7;
+        int index = binarySearch(arr, target);
+        if (index != -1) {
+            System.out.println("Element found at index " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
