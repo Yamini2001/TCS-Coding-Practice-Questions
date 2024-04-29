07f73ed0dafa5a5b70e43eebbf7db110a6d4818f
BeSingularTeam <besingulardemo@gmail.com>
2024-04-29 20:49:33 +0530

    update

--- Strings/FizzBuzz.java
+++ Strings/FizzBuzz.java
@@ -1,21 +1,21 @@
-import java.util.List;
-import java.util.ArrayList;
-class Solution {
-    public List<String> fizzBuzz(int n) {
-        List<String> answer = new ArrayList<>();
-        
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                answer.add("FizzBuzz");
-            } else if (i % 3 == 0) {
-                answer.add("Fizz");
-            } else if (i % 5 == 0) {
-                answer.add("Buzz");
-            } else {
-                answer.add(String.valueOf(i));
+import java.util.*;
+class Main{
+    public static List<String> FizzBuzz(int n){
+        List<Integer> answer = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                answer.add(Integer.valueOf("FizzBuzz"));
+            }
+            else if(i%3==0){
+                answer.add(Integer.valueOf("Fizz"));
+            }
+            else if(i%5==0){
+                answer.add(Integer.valueOf("Buzz"));
+            }
+            else{
+                answer.add(Integer.valueOf(String.valueOf(i)));
             }
         }
-        
         return answer;
-    }
+    } 
 }
