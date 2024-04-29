8110ec5b455e473e2d4de80f3186f1b59a5ff84e
BeSingularTeam <besingulardemo@gmail.com>
2024-04-29 23:58:44 +0530

    Pattern2

---
+++ CodesDope practice/Pattern2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+           for(int j = 1;j<=2*i-1;j++){
+               System.out.print("*");
+           }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j = 1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
