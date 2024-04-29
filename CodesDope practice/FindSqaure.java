817edbadc890aa838903f17c81594c7eefc4f1dd
BeSingularTeam <besingulardemo@gmail.com>
2024-04-30 00:12:53 +0530

    FindSqaure

---
+++ CodesDope practice/FindSqaure.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        float n = sc.nextFloat();
+        float square = n*n;
+        System.out.println("Square: "+square);
+
+    }
+}
