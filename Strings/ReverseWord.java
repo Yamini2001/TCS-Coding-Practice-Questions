467b1b4d69fadb3ef134e3798dcc3ed296dfbf9e
BeSingularTeam <besingulardemo@gmail.com>
2024-05-02 00:37:13 +0530

    ReverseWord

---
+++ Strings/ReverseWord.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        String s1 = "India" , s2 = " ";
+        System.out.println("Original Words: ");
+        System.out.println("India");
+        for(int i=0;i<s1.length();i++){
+            char ch = str.charAt(i);
+            s2 = ch+s2;
+        }
+        System.out.println("Reverse Word: "+s2);
+    }
+}
