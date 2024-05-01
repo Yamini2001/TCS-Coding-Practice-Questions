ad5ac523f58ef4fec351caef6b8f61d86db18fbf
BeSingularTeam <besingulardemo@gmail.com>
2024-05-01 23:48:40 +0530

    convertToTittle

--- Strings/ExcelSheetColumnTitle.java
+++ Strings/ExcelSheetColumnTitle.java
@@ -1,26 +1,12 @@
-class Solution {
-public String convertToTitle(int columnNumber) {
-        StringBuilder sb = new StringBuilder();
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        if(columnNumber < 27){
-            return String.valueOf(alphabet.charAt(columnNumber-1));
+class Main{
+    public String convertToTitle(int columnNumber){
+        StringBuilder title = new StringBuilder();
+        while(columnNumber >0){
+            columnNumber--;
+            int remainder = columnNumber % 26;
+            title.insert(0, (char) ('A' + remainder));
+            columnNumber /= 26;
         }
-       
-        while(columnNumber > 26){
-            int temp = columnNumber%26;
-            if (temp > 0){
-                sb.append(alphabet.charAt(temp-1));
-                columnNumber /= 26;
-            }
-            else{
-                sb.append('Z');
-                columnNumber /= 26;
-                columnNumber -= 1;
-            }
-
-        }
-        sb.append(alphabet.charAt(columnNumber-1));
-       
-        return String.valueOf(sb.reverse());
+        return title.toString();
     }
-};
+}
